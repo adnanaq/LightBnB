167c9a51456578ff620f915f3cdc5397990143fa
Dani <dreamboy.dani@gmail.com>
2020-09-17 15:30:59 -0600

    Add promise to the AddProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -52,10 +52,10 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function ({ name, email, password }) {
+const addUser = function (user) {
   const queryStr = {
     text: `INSERT INTO users (name, email, password) VALUES ( ($1, $2, $3) RETURNING *;`,
-    values: [name, email, password]
+    values: [{ name, email, password } = user]
   }
 
   return pool
@@ -125,10 +125,61 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function ({
+  owner_id,
+  title,
+  description,
+  thumbnail_photo_url,
+  cover_photo_url,
+  cost_per_night,
+  street,
+  city,
+  province,
+  post_code,
+  country,
+  parking_spaces,
+  number_of_bathrooms,
+  number_of_bedrooms
+}) {
+  const queryStr = {
+    text: `INSERT INTO properties
+    (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`,
+    values: [
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    ]
+  };
+
+  return pool
+    .query(queryStr)
+    .then(res => res.rows[0])
 }
 exports.addProperty = addProperty;
